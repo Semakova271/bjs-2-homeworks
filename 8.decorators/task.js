d6e58ffdd1bf1110898f6549dfba3efb2cf9219f
Irina Semakova <Semakovais@yandex.ru>
2024-12-14 16:38:34 +0300

    Восьмое задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,50 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+    let cache = [];
   
-}
-
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
+    function wrapper(...args) {
+      const hash = md5(JSON.stringify(args));
+      const objectInCache = cache.find((item) => item.hash === hash);
   
-}
+      if (objectInCache) {
+        console.log("Из кеша: " + objectInCache.value);
+        return "Из кеша: " + objectInCache.value;
+      }
+  
+      let result = func(...args);
+      cache.push({ hash, value: result });
+      if (cache.length > 5) {
+        cache.shift();
+      }
+  
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+    return wrapper;
+  }
+  
+  //Задача № 2
+  function debounceDecoratorNew(func, delay) {
+    let timeoutId = null;
+    function wrapper(...args) {
+      if (timeoutId === null) {
+        func(...args);
+        wrapper.count++;
+      }
+  
+      if (timeoutId) {
+         clearTimeout(timeoutId);
+      }
+  
+      timeoutId = setTimeout(() => {
+        timeoutId = true;
+        func(...args);
+        wrapper.count++;
+      }, delay);
+      wrapper.allCount++;
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
+  }
+  
